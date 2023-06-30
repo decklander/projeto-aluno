3bd189598454485a9bb7294ca36e4960c9ec055f
decklander <deckardcain20@gmail.com>
2023-06-30 18:52:59 -0300

    atualização realizada

---
+++ config.js
@@ -0,0 +1,8 @@
+var footerConfig = "Faculdade Anhanguera de Imperatriz - Rua Barão do Rio Branco, nº 300 - Centro, Imperatriz - MA, CEP 65900-430";
+var footerConfigTel = "Tel.:(99) 2101-6000";
+
+
+document.getElementById('footerConfig').innerHTML = footerConfig;
+document.getElementById('footerConfigTel').innerHTML = footerConfigTel;
+
+
